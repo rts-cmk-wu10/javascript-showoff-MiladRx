a35ee4103000eca9fc7f8d788940f8905300f830
Milad <miladroubei70@gmail.com>
2023-09-01 13:29:48 +0200

    Final

--- app.js
+++ app.js
@@ -1,1 +1,69 @@
-// skriv din JavaScript her...
+
+const opgaveInput = document.getElementById('opgaveInput');
+const addKnap = document.getElementById('addKnap');
+const opgaveListe = document.getElementById('opgaveListe');
+
+// Opret et array til at opbevare opgaver
+const opgaver = [];
+
+// Funktion til at tilføje en opgave
+function tilføjOpgave() {
+    const opgaveTekst = opgaveInput.value;
+
+    opgaver.push({ tekst: opgaveTekst, udført: false });
+    opgaveInput.value = '';
+
+    // Opdater opgave listeten
+    visOpgaver();
+}
+
+// Funktion til at fjerne en opgave
+function fjernOpgave(index) {
+    opgaver.splice(index, 1);
+    visOpgaver();
+}
+
+// Funktion til at markere en opgave som udført
+function markerOpgave(index) {
+    opgaver[index].udført = !opgaver[index].udført;
+    visOpgaver();
+}
+
+// Funktion til at vise opgaver i brugergrænsefladen
+function visOpgaver() {
+    // Ryd den nuværende liste
+    opgaveListe.innerHTML = '';
+
+    // Tilføj hver opgave til listen
+    for (let i = 0; i < opgaver.length; i++) {
+        const opgave = opgaver[i];
+        const listeElement = document.createElement('li');
+
+        // Opret en afkrydsningsboks til at markere opgaven som udført
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = opgave.udført;
+        checkbox.addEventListener('change', () => markerOpgave(i));
+
+        // Opret en knap til at fjerne opgaven
+        const fjernKnap = document.createElement('button');
+        fjernKnap.textContent = 'Fjern';
+        fjernKnap.addEventListener('click', () => fjernOpgave(i));
+
+        // Opret en div til opgaveTeksten
+        const opgaveTekstDiv = document.createElement('div');
+        opgaveTekstDiv.textContent = opgave.tekst;
+
+        listeElement.appendChild(checkbox);
+        listeElement.appendChild(opgaveTekstDiv);
+        listeElement.appendChild(fjernKnap);
+        opgaveListe.appendChild(listeElement);
+
+
+    }
+}
+
+// Tilføj en klikhændelseslytter til "Tilføj Opgave"-knappen
+addKnap.addEventListener('click', tilføjOpgave);
+
+
---
+++ cookie.js
@@ -0,0 +1,16 @@
+const cookieKnap = document.getElementById('cookie-knap');
+const cookieAntal = document.getElementById('cookie-antal');
+
+let cookies = 0;
+
+cookieKnap.addEventListener('click', () => {
+    // 1 Cookie per klik
+    cookies++;
+    cookieAntal.textContent = cookies;
+});
+
+// Producere 5 Cookies per 3sek automatisk
+setInterval(() => {
+    cookies += 5;
+    cookieAntal.textContent = cookies;
+}, 3000);
